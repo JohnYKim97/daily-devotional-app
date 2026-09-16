582cff8faf522f27de7035f5258f5c5050caa46f
John Kim <johnykim1997@gmail.com>
2026-09-16 13:17:11 -0400

    CSS: navbar + fixing global css sheet

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject, viewChild } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 import { AuthService } from '../../core/services/auth.service';
 import { DateService } from '../../core/services/date.service';
@@ -7,7 +7,7 @@ import { ImportScheduleComponent } from '../../features/admin/import-schedule/im
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, ImportScheduleComponent],
+  imports: [RouterLink, RouterLinkActive, ImportScheduleComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
