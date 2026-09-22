93193af3dcbd84841eb93c82b142a90139cde5c6
John Kim <johnykim1997@gmail.com>
2026-09-22 17:36:59 -0400

    Styling for login page + removed navbar from login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { ToastComponent } from './shared/toast/toast.component';
+import { AuthService } from './core/services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +13,5 @@ import { ToastComponent } from './shared/toast/toast.component';
 })
 export class AppComponent {
   protected readonly title = signal('daily-devotional-app');
+  protected authService = inject(AuthService);
 }
--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 
 import { AuthService } from '../../../core/services/auth.service';
+import { LighthouseIconComponent } from '../../../shared/lighthouse-icon/lighthouse-icon.component';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [LighthouseIconComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
